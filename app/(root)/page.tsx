e0ed73a64b91e6310d33e0f082b2248dc8aad852
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-05-27 11:20:31 +0530

    add homepage ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox';
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn={ firstName: 'Kirandeep'}
   return (
-    <div>
-         
-    </div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext="Access and manage your account and transactions efficiently."
+                />
+                <TotalBalanceBox
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={1250.35}
+                 />
+            </header>
+        </div>
+      
+    </section>
   )
 }
 
